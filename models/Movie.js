d42ee17d3d77d62acd1e04cf21d0d0bbdeea7af3
mirjSolution <ricojohndatoon23@gmail.com>
2020-10-30 06:43:16 -0400

    Create model for movies

--- models/Movie.js
+++ models/Movie.js
@@ -2,21 +2,21 @@ const mongoose = require('mongoose');
 
 // Database schema
 let MovieSchema = mongoose.Schema({
-    title: {type: String, required: true},
-    description: {type: String, required: true},
-    genre: {
-      name: String,
-      description: String
-    },
-    director: {
-      name: String,
-      bio: String,
-      birth: String,
-      death: String
-    },
-    actors: [String],
-    imagePath: String,
-    featured: Boolean
-  });
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  genre: {
+    name: String,
+    description: String,
+  },
+  director: {
+    name: String,
+    bio: String,
+    birth: String,
+    death: String,
+  },
+  actors: [String],
+  imagePath: String,
+  featured: Boolean,
+});
 
-  module.exports= mongoose.model('Movie', MovieSchema);
+module.exports = mongoose.model('Movie', MovieSchema);
