77c46e5251774a61d2da3590820aa9cd043eaa03
mirjSolution <ricojohndatoon23@gmail.com>
2020-10-30 06:46:38 -0400

    Import files

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
+const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
 
 // Connect to database
 connectDB();
 
+// Route files
+const movies = require('./routes/movies');
+
 const app = express();
+
+// Body parser
+app.use(bodyParser.json());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Mount routers
+app.use('/movies', movies);
 
 // Listen to connection
 const port = process.env.PORT || 8080;
-  app.listen(port, '0.0.0.0',() => {
+app.listen(port, '0.0.0.0', () => {
   console.log('Listening on Port ' + port);
 });
-
