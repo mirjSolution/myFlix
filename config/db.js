dd93042d026374229dfe98f1a675081c92f87959
mirjSolution <ricojohndatoon23@gmail.com>
2020-10-30 16:42:59 -0400

    fix eslint errors

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
-const connectDB = async() => {
-    const conn = await mongoose.connect('mongodb+srv://myFlixDBadmin:GflAIAUomcS4L7EX@myflixdb.le9cb.mongodb.net/myFlixDB?retryWrites=true&w=majority', {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true
-    })
-    console.log(`MongoDB Connected: ${conn.connection.host}`)
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.CONNECTION_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+  console.log(`MongoDB Connected: ${conn.connection.host}`);
 };
 
 module.exports = connectDB;
