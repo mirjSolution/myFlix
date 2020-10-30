7ab90d23bb462bf1a9be1cb24cfa9a27dfa00f22
mirjSolution <ricojohndatoon23@gmail.com>
2020-10-30 15:03:20 -0400

    Separate model users code

--- models/User.js
+++ models/User.js
@@ -2,19 +2,19 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 let UserSchema = mongoose.Schema({
-    username: {type: String, required: true},
-    password: {type: String, required: true},
-    email: {type: String, required: true},
-    birthday: Date,
-    favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  birthday: Date,
+  favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
 });
 
 UserSchema.statics.hashPassword = (password) => {
-    return bcrypt.hashSync(password, 10);
+  return bcrypt.hashSync(password, 10);
 };
 
-UserSchema.methods.validatePassword = function(password) {
-    return bcrypt.compareSync(password, this.Password);
-}
+UserSchema.methods.validatePassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
 
 module.exports = mongoose.model('User', UserSchema);
