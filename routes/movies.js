e3fffb32c6de4762d00466bf4b65c88be0e04fcb
mirjSolution <ricojohndatoon23@gmail.com>
2020-11-03 17:25:30 -0500

    Add localhost-1234 to access api

--- routes/movies.js
+++ routes/movies.js
@@ -9,9 +9,7 @@ const {
 
 const router = express.Router();
 
-router
-  .route('/')
-  .get(passport.authenticate('jwt', { session: false }), getMovies);
+router.route('/').get(getMovies);
 router
   .route('/:title')
   .get(passport.authenticate('jwt', { session: false }), getMovie);
