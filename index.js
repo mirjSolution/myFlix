4bf7513a5a4a486b2ed04679ab3c86eeb596e5d5
mirjSolution <ricojohndatoon23@gmail.com>
2020-11-19 08:27:03 -0500

    Deploy

--- index.js
+++ index.js
@@ -23,24 +23,24 @@ const users = require('./routes/users');
 app.use(express.json());
 
 // Require cors module
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234'];
-// app.use(cors()); // allow requests from all origins
+app.use(cors()); // allow requests from all origins
+// let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234'];
 // only certain origins to be given access
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn’t found on the list of allowed origins
-        let message =
-          'The CORS policy for this application doesn’t allow access from origin ' +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         // If a specific origin isn’t found on the list of allowed origins
+//         let message =
+//           'The CORS policy for this application doesn’t allow access from origin ' +
+//           origin;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
